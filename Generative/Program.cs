6fbc7ab276c19d1dade24e1fc6c0c3db77ca32ec
Azerlegend <azarak@code.edu.az>
2023-02-07 18:52:47 +0400

    Generative

---
+++ Generative/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+  namespace test
+  {
+    public class Program
+    {
+        static void Main()
+        {
+            FindType<int>(5);
+            SwapTypes<int>(5, 4);
+        }
+
+        public static void FindType<T>(T Type)
+            {
+            Console.WriteLine(Type);
+
+            }
+        
+
+        public static void SwapTypes<T>(T Type1, T Type2)
+        {
+            T Type3 =  default;
+            T temporary = Type3;
+            Type3 = Type1;
+            Type1 = Type2;
+            Type2 = Type3;
+            Console.WriteLine($"{Type1} {Type2}");
+
+
+
+        }
+
+
+
+    }
+
+
+
+  }
+
+
+
+
+
+
+
+
